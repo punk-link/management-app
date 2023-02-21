e4c3afb3ab4e5945896e2ddde9424f0d1a47cf98
Kirill <pavlovk7420@gmail.com>
2023-02-22 00:57:12 +0400

    Deleted previous design page-type

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.scss';
 import Account from './components/account/account';
 import Grid from './components/common/grid'
-import Header from './components/header/header';
 import { Routes, Route } from "react-router-dom";
 import ScreenContainer from './components/common/screen-container';
 import Login from './components/login/login';
 import SignUp from './components/signup/sign-up';
 import routes from './constants/routes';
 
-//import SecondaryScreenContainer from './components/common/secondary-screen-container';
-
 function App() {
     return (
         <Grid>
-            <Header />
+            {/* <Header /> */}
             <ScreenContainer>
                 <Routes>
                     <Route path={routes.login} element={<Login />} />
